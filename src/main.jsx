32b6480bd99c994c21c06d4e01cf50cd4ba352e5
Rithish-FullStack <rithishpg0@gmail.com>
2025-05-29 15:20:36 +0530

    demo-8

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.jsx'; // Adjust the path if needed
 import LoadingPage from './components/LoadingPage.jsx'; // Make sure the path is correct
 
