6c3cf132b70a9126f26879f30dbd6c07f1bd9306
preethi01OCT <jbpreethi03@gmail.com>
2025-10-31 14:44:30 +0530

    comma missign in app.py

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ CORS(app)
 
 # 📧 Zoho Mail Configuration (Testing Mode)
 app.config.update(
-    MAIL_SERVER='smtp.zoho.in'
-    MAIL_PORT=587
-    MAIL_USE_SSL=True
+    MAIL_SERVER='smtp.zoho.in',
+    MAIL_PORT=587,
+    MAIL_USE_SSL=True,
     MAIL_USERNAME='preethi.jb@kodivian.com',
     MAIL_PASSWORD='ajRMC3TdYZrs',  # <-- paste this app password exactly (no spaces)
     MAIL_DEFAULT_SENDER='preethi.jb@kodivian.com'
 )
+
 
 
 mail = Mail(app)
